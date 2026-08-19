3fe4800d4ce16a2bd852e72dd387c899a35dc61e
Stratix User <stratixuser@example.com>
2026-08-19 21:34:47 +0500

    Load Firebase credentials from env variable

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
 const crypto = require("crypto");
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore, FieldValue } = require("firebase-admin/firestore");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 initializeApp({
   credential: cert(serviceAccount),
